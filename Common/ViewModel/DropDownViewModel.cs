6e6cc40ef426d358f6219c06f770e55a37c3253c
ALI ASM IFTEKHAR <ali.iftekhar343@gmail.com>
2018-05-26 23:00:18 +0600

    Common Class Added

---
+++ Common/ViewModel/DropDownViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common.ViewModel
+{
+    public class DropDownViewModel
+    {
+        public string Id { get; set; }
+        public string Text { get; set; }
+        public object Data { get; set; }
+    }
+}
